ad76fa367862f5d5dc77c89840949f3e609aba0e
OPS-NeoRetro <familyorang0@gmail.com>
2025-11-29 21:47:23 +0800

    Update bitsperlong.h

--- arch/x86/include/uapi/asm/bitsperlong.h
+++ arch/x86/include/uapi/asm/bitsperlong.h
@@ -2,13 +2,17 @@
 #ifndef __ASM_X86_BITSPERLONG_H
 #define __ASM_X86_BITSPERLONG_H
 
-#if defined(__x86_64__) && !defined(__ILP32__)
-# define __BITS_PER_LONG 64
-#else
-# define __BITS_PER_LONG 32
+#define __BITS_PER_LONG 64
+
+/* SPDX-License-Identifier: GPL-2.0 WITH Linux-syscall-note */
+#ifndef _UAPI__ASM_GENERIC_BITS_PER_LONG
+#define _UAPI__ASM_GENERIC_BITS_PER_LONG
+
+#ifndef __BITS_PER_LONG_LONG
+#define __BITS_PER_LONG_LONG 64
 #endif
 
-#include <asm-generic/bitsperlong.h>
+#endif /* _UAPI__ASM_GENERIC_BITS_PER_LONG */
 
 #endif /* __ASM_X86_BITSPERLONG_H */
 
